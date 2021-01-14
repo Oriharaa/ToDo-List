ea7f2418376f6565e98aa3691a456fe2ec93f9aa
Oriharara <60921094+Oriharaa@users.noreply.github.com>
2021-01-14 14:48:12 +0900

    JS : Save name, email, password

--- main.js
+++ main.js
@@ -3,19 +3,30 @@
 const ask = document.querySelector('.home__text'); 
 const input = document.querySelector('.home__input');
 const homeFooter = document.querySelector('.home__footer');
+const homeContinue = document.querySelector('.home__continue');
+
 const USER_LS = 'currentUser';
-let currentUser = undefined;
+const USER_EM = 'currentEmail';
+const USER_PSW = 'currentPassword';
+
+let state = 'name';
 
 input.addEventListener('keydown', (e)=>{
     if(e.keyCode === 13){
-        if(currentUser === undefined){
-            saveName(e);
-            loadName();
-        }else {
-            emailFromPsw();
+        switch(state){
+            case 'name' : 
+                saveName(e);
+                loadName();
+                break;
+            case 'email' : 
+                saveEmail(e);
+                emailFromPsw();
+                break;
+            case 'password' : 
+                savePassword(e);
+                break;
         }
     }
-
 });
 
 
@@ -29,12 +40,14 @@ function nameFromEmail(text){
     homeFooter.appendChild(changeName);
     homeFooter.appendChild(loggedOut);
 
+    input.value = '';
+    state = 'email';
 }
 
 function emailFromPsw(){
     const terms = document.createElement('p');
     const termsLink = document.createElement('a');
-    const homeContinue = document.querySelector('.home__continue');
+    const prevEmail = document.createElement('button');
     terms.innerText = 'Momentum respects your privacy. Learn more in our ';
     terms.classList.add('home__terms');
     termsLink.innerText = 'Terms & Privacy Policy.';
@@ -42,25 +55,39 @@ function emailFromPsw(){
     termsLink.setAttribute('href', 'https://momentumdash.com/legal');
     termsLink.setAttribute('target', 'blank');
     ask.innerText = 'Please choose a password.';
+    prevEmail.innerText = 'Email';
     input.style.width = '650px';
     homeContinue.before(terms);
     terms.appendChild(termsLink);
+    
+    input.value = '';
+    state = 'password';
+    
+    const changeName = homeFooter.firstChild;
+    const loggedOut = homeFooter.lastChild;
+    
+    homeFooter.removeChild(changeName);
+    homeFooter.removeChild(loggedOut);
+    homeFooter.appendChild(prevEmail);
 
 };
 
 function saveName(e){
     const userName = e.target.value;
     localStorage.setItem(USER_LS, userName);
+}
 
+function saveEmail(e){
+    const userEmail = e.target.value;
+    localStorage.setItem(USER_EM, userEmail);
+}
 
-
+function savePassword(e){
+    const userPassword = e.target.value;
+    localStorage.setItem(USER_PSW, userPassword);
 }
 
 function loadName(){
-    currentUser = localStorage.getItem(USER_LS);
-    if(currentUser === null){
-        // 비어있다면..
-    }else {
+    const currentUser = localStorage.getItem(USER_LS);
         nameFromEmail(currentUser);
-    }
 };
